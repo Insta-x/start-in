d899c3b991351772c9e84539d85f4c9d29938228
Rhfialyndra <rahfialyendra5@gmail.com>
2022-10-23 22:20:32 +0700

    done setup

--- inforum/urls.py
+++ inforum/urls.py
@@ -5,5 +5,6 @@ from .views import *;
 app_name = 'inforum'
 
 urlpatterns = [
-    path('', show_all_forum, name='show_all_forum')
+    path('', show_all_forum, name='show_all_forum'),
+    path('get_all_forum/', get_all_forum, name='get_all_forum'),
 ]
