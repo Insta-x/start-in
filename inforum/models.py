42e7ba42853a68641c9a631e0ffa73a05a1a55b7
Insta-x <uttsadajason@outlook.com>
2022-10-25 21:21:35 +0700

    fix migrations and date

--- inforum/models.py
+++ inforum/models.py
@@ -5,16 +5,16 @@ from authentication.models import User
 # Create your models here.
 
 class Forum(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE);
-    username= models.CharField(max_length=100);
-    time_created = models.DateField(default=timezone.now());
-    title = models.CharField(max_length=100);
-    content = models.TextField(max_length=1000);
-    category = models.CharField(max_length=20);
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    username= models.CharField(max_length=100)
+    time_created = models.DateField(default=timezone.now)
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000)
+    category = models.CharField(max_length=20)
 
 
 class Comment(models.Model):
-    forum = models.ForeignKey("Forum", on_delete=models.CASCADE);
-    from_user = models.ForeignKey(User, on_delete=models.CASCADE);
-    time_created = models.DateField(default=timezone.now());
-    comment = models.TextField(max_length=250);
+    forum = models.ForeignKey("Forum", on_delete=models.CASCADE)
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_created = models.DateField(default=timezone.now)
+    comment = models.TextField(max_length=250)
