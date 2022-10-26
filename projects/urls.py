877013bca5d5153abbf25690433e09e5362b1c7f
Insta-x <uttsadajason@outlook.com>
2022-10-26 20:30:47 +0700

    working search bar

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from pydoc import Helper
 from django.urls import path
-from .views import show_projects
+from .views import show_projects, get_projects
 
 app_name = 'projects'
 
 urlpatterns = [
-    path('', show_projects, name='show_projects')
+    path('', show_projects, name='show_projects'),
+    path('get-projects/', get_projects, name='get_projects')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,16 @@
+from turtle import title
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.core import serializers
 from requests import request
+from .models import Project
 
 def show_projects(request):
     context = {'logged_in' : request.user.is_authenticated}
 
     return render(request, 'show_projects.html', context)
+
+def get_projects(request):
+    data = Project.objects.filter(title__icontains=request.GET.get('search'))
+
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
