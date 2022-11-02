b0eb82938736e3a8ebf1a57babcdffb1706671f7
Mahmud Asrul <mahmud.asrul@ui.ac.id>
2022-11-02 23:30:54 +0700

    bismillah yaallah alhamdulillah allahuakbar

--- courses/views.py
+++ courses/views.py
@@ -3,4 +3,9 @@ from .models import Course, Course_detail
 
 def show_courses(request):
     context = {'logged_in' : request.user.is_authenticated}
-    return render(request, 'course.html', context)
+    return render(request, 'courses.html', context)
+
+def course(request):
+    courses = Course.objects.all()
+    context = {'courses': courses}
+    return render(request,'courses.html', context)
