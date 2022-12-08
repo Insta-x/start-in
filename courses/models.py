3a8efb99019f5faffb06e5d2a4719989405c5996
Mahmud Asrul <mahmud.asrul@ui.ac.id>
2022-12-08 18:15:17 +0700

    adding migration

--- courses/models.py
+++ courses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Course(models.Model):
     course_name = models.CharField(max_length=200)
-    course_url = models.URLField(blank=False, null=False)
+    course_url = models.URLField(blank=True, null=False)
     thumbnail = models.ImageField(null=False, blank=True)
     
     def __str__(self):
