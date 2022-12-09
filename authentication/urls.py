d6abd23b984cdfc88fcd01dcc39fd53885938504
Insta-x <uttsadajason@outlook.com>
2022-12-09 21:15:26 +0700

    api logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register, login_user, logout_user, api_login
+from .views import register, login_user, logout_user, api_login, api_logout
 
 app_name = 'authentication'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('api/login/', api_login, name='api_login'),
+    path('api/logout/', api_logout, name='api_logout'),
 ]
